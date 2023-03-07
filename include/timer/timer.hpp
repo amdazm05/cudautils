fcc9c8ec33d4dd9f2826b7d7bc15c45a72fef942
amdazm05 <amdali1998@yahoo.com>
2023-03-07 23:24:29 +0500

    feat: updated timer scheme

--- include/timer/timer.hpp
+++ include/timer/timer.hpp
@@ -3,16 +3,52 @@
 
 #include "utils/util.hpp"
 
+using system_stopwatch = std::chrono::system_clock;
+using monotonic_stopwatch = std::chrono::steady_clock;
+template<class TypeofClock, class Duration>
 class Timer
 {
     public:
         Timer();
         void startTimer();
         void stopTimer();
+        double getTime();
         ~Timer();
     private:
-        using system_stopwatch = std::chrono::system_clock;
-        using monotonic_stopwatch = std::chrono::steady_clock;
+        TypeofClock start;
+        TypeofClock stop;
+        Duration elapsedTime;
 };
+
+template<class TypeofClock, class Duration>
+Timer<T>::Timer()
+{
+
+}
+
+template<class TypeofClock, class Duration>
+void Timer<T>::startTimer()
+{
+    start = T::now();
+}
+
+template<class TypeofClock, class Duration>
+double Timer<TypeofClock,Duration>::getTime()
+{
+    return elapsedTime.count;
+}
+
+template<class TypeofClock, class Duration>
+void Timer<TypeofClock,Duration>::stopTimer()
+{
+    stop = T::now();
+    elapsedTime = start - stop;
+}
+
+template<class TypeofClock, class Duration>
+Timer<TypeofClock,Duration>::~Timer()
+{
+
+}
 
 #endif //_TIMER
